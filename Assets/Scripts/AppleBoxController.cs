e8b01dfa07dea4caefa1c2a26051b63737467957
Godzzo <egodzzo@gmail.com>
2020-03-30 17:18:53 +0100

    Show numbers with boxes

---
+++ Assets/Scripts/AppleBoxController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AppleBoxController : MonoBehaviour
+{
+    AppleController[] _apples;
+
+    public int appleCount = 5;
+    public int position = 1;
+
+    public TextMeshPro numberText;
+    public TextMeshPro positionText;
+
+    void Start()
+    {
+        _apples = GetComponentsInChildren<AppleController>();
+
+        positionText.SetText(position + ".");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetVisibleApples(appleCount);
+    }
+
+    public void SetVisibleApples(int apples)
+    {
+        numberText.SetText(apples.ToString());
+
+        foreach (AppleController apple in _apples)
+        {
+            apple.gameObject.SetActive(!(apple.position > appleCount));
+        }
+    }
+}
---
+++ Assets/Scripts/AppleBoxesController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AppleBoxesController : MonoBehaviour
+{
+    AppleBoxController[] _boxes;
+
+    public TextMeshProUGUI numberText;
+
+    int _baseNumber = 6;
+    int _times = 7;
+
+
+    void Start()
+    {
+        _boxes = GetComponentsInChildren<AppleBoxController>();
+
+        SetQuestion();
+    }
+
+    void SetQuestion()
+    {
+        _baseNumber = Random.Range(1, 10);
+        _times = Random.Range(1, 10);
+
+        numberText.SetText(_baseNumber.ToString() + " * " + _times.ToString() +" = ");
+
+        foreach (AppleBoxController box in _boxes)
+        {
+            if (box.position < _times+1)
+            {
+                box.appleCount = _baseNumber;
+            } 
+            else
+            {
+                box.appleCount = 0;
+            }
+            
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
